2f33049a7b3327b274a46820f0b5e762199e1b2c
Johannes Thomsen <43598221+joha321j@users.noreply.github.com>
2021-10-02 20:31:41 +0200

    Initial Commi

---
+++ TestDataHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDataHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
